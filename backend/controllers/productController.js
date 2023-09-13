c20bc77e7070b1f43956e6d94b7a4612b1bb5e77
PeeyushRawat <peeyushbhaio@gmail.com>
2023-09-13 20:12:53 +0530

    Adding Filter, Pagination, search

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,6 +2,7 @@ const Product = require('../models/product')
 
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors')
+const APIFeatures = require('../utils/apiFeatures');
 
 //Create new product => /api/v1/admin/product/new
 exports.newProduct = catchAsyncErrors( async (req, res, next) => {
@@ -16,12 +17,19 @@ exports.newProduct = catchAsyncErrors( async (req, res, next) => {
 // Get all products => /api/v1/products
 exports.getProducts = catchAsyncErrors( async (req, res, next) => {
 
-    const products = await Product.find();
+    const resPerPage = 4;
+    const productCount = await Product.countDocuments();
+    const apiFeatures = new APIFeatures(Product.find(), req.query)
+                            .search()
+                            .filter()
+                            .pagination(resPerPage);
+    const products = await apiFeatures.query;
 
     res.status(200).json({
         success: true,
         // message: 'This route will show all products in database.'
         count: products.length,
+        productCount,
         products
     })
 })

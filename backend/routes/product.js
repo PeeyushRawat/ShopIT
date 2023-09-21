618b81d696aa38b1d242068ea6cb02a46257c176
PeeyushRawat <peeyushbhaio@gmail.com>
2023-09-22 04:13:55 +0530

    Adding User Reviews

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -6,7 +6,10 @@ const {
     newProduct, 
     getSingleProduct, 
     updateProduct,
-    deleteProduct
+    deleteProduct,
+    createProductReview,
+    getProductReviews,
+    deleteReview
 
 } = require('../controllers/productController')
 
@@ -22,5 +25,10 @@ router.route('/admin/product/new').post( isAuthenticatedUser, authorizeRoles('ad
 router.route('/admin/product/:id')
                 .put( isAuthenticatedUser, authorizeRoles('admin'), updateProduct)
                 .delete( isAuthenticatedUser, authorizeRoles('admin'), deleteProduct);
+
+
+router.route('/review').put(isAuthenticatedUser, createProductReview)
+router.route('/reviews').get(isAuthenticatedUser, getProductReviews)
+router.route('/reviews').delete(isAuthenticatedUser, deleteReview)
 
 module.exports = router;
